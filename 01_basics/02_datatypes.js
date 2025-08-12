5ba3ccfbe4afac7317851b3812da65a78ffb344f
Areeb161 <mianareeb007@gmail.com>
2025-08-12 11:12:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all js code as a new version
+// alert (3 +3 ) // using nodejs , not browser
+
+let name = "Areeb"
+let age =22
+let isLoggedIn = false
+
+// datatypes numbers , string , boolean , null , symbol => unique
+// null is a object in javascript
+// undefined is undefined
