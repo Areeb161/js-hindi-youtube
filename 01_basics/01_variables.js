7bcf9322cf556f85e2c5d3fda6c541746a30ba1a
Areeb161 <mianareeb007@gmail.com>
2025-08-12 10:53:23 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 144553
+let accountEmail = "areeb@google.com"
+var accountPassword = 12345
+accountCity = "Islamabad"
+let accountState
+// accountID = 4
+
+accountEmail = "shahid@google.com"
+accountPassword = 3214
+accountCity = "RwalaPindi"
+
+console.table( [accountID, accountEmail, accountPassword, accountCity, accountState] )
+
